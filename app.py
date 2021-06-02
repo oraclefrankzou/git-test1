c20c0d63c8e14d1f2ae49cd829a69daf1fbb66fd
oraclefrankzou <34939385+oraclefrankzou@users.noreply.github.com>
2021-06-02 12:36:50 +0800

    Create app.py
    init file

---
+++ app.py
@@ -0,0 +1,15 @@
+import socket
+from flask import Flask
+import json
+import datetime
+
+
+app=Flask(__name__)
+
+@app.route('/')
+def hello():
+     return json.dumps({'date12': datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),'host':socket.gethostname()})
+
+
+if __name__ == '__main__':
+   app.run(host='0.0.0.0')
